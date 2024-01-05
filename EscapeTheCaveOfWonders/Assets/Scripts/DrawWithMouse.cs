c5b33a2b54990da50e9a6d7045c8411d2558bdf4
TiffanyN22 <tnguyen22@ndsj.org>
2024-01-05 14:17:04 -0800

    Removed drawing code from versions that didn't work

--- EscapeTheCaveOfWonders/Assets/Scripts/DrawWithMouse.cs
+++ EscapeTheCaveOfWonders/Assets/Scripts/DrawWithMouse.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// using Radishmouse;
 
 public class DrawWithMouse : MonoBehaviour
 {
@@ -11,7 +10,6 @@ public class DrawWithMouse : MonoBehaviour
     private Vector2 center;
 
     private void Start(){
-        // line = GetComponent<LineRenderer>();
         previousPosition = transform.position;
     }
 
@@ -20,20 +18,11 @@ public class DrawWithMouse : MonoBehaviour
         if(Input.GetMouseButton(0)){
             Debug.Log("Drawing");
             Vector2 currentPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            // Vector2 drawPos = new Vector2((currentPosition.x * 119.08f + 413f), (currentPosition.y * -35.5f + 262f));
             Debug.Log("Current Position" + currentPosition);
             
-
-            //based on center
-            // Vector2 panelCenter = transform.position;
-            // Debug.Log("Center position: " + panelCenter);
-            // Debug.Log("Dist from center: " + (currentPosition - panelCenter));
-            // Vector2 drawPos = new Vector2((currentPosition.x * 32f + 13224.64f), (currentPosition.y * 31.75f + 8308.57f));
-
-             Vector2 drawPos = new Vector2((currentPosition.x * 32f), (currentPosition.y * 30.85f -5.24f));
+            Vector2 drawPos = new Vector2((currentPosition.x * 32f), (currentPosition.y * 30.85f -5.24f));
 
             if(Vector2.Distance(currentPosition, previousPosition) > minDistance){
-                // line.points = line.points.Append(currentPosition).ToArray();
                 line.points.Add(drawPos);
                 line.SetAllDirty();
                 previousPosition = currentPosition;
