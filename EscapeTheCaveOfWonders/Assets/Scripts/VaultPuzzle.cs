0fa27127e7c26eabe7a1ec7986ff49ee79c82d09
TiffanyN22 <tnguyen22@ndsj.org>
2024-01-06 01:22:38 -0800

    Added sundial art and gem drop with correct time

--- EscapeTheCaveOfWonders/Assets/Scripts/VaultPuzzle.cs
+++ EscapeTheCaveOfWonders/Assets/Scripts/VaultPuzzle.cs
@@ -6,17 +6,35 @@ public class VaultPuzzle : MonoBehaviour
 {
     [SerializeField] Transform dial1;
     [SerializeField] Transform dial2;
-    private float dial1Target = -0.539f;
-    private float dial2Target = 0.715f;
+    private float dial1Target = 0.8765f;
+    private float dial2Target = -0.708f;
+    private bool droppedGem = false;
 
     void Update()
     {
+        if(droppedGem){
+            return;
+        }
         if(closeToAngle(dial1.rotation.z, dial1Target) && closeToAngle(dial2.rotation.z, dial2Target)){
             Debug.Log("dial success!");
+            DropGem();
         }
     }
 
     private bool closeToAngle(float dialAngle, float targetAngle){
+        // Debug.Log(dialAngle);
         return Mathf.Abs(dialAngle-targetAngle) < 0.1;
     }
+
+    private void DropGem()
+    {
+        Item gem = GameManager.instance.itemManager.GetItemByName("Green Gem");
+        Vector2 spawnLocation = transform.position;
+        Vector2 spawnOffset = new Vector2(1f, Random.Range(-1f, 0f));
+
+        Item droppedItem = Instantiate(gem, spawnLocation + spawnOffset, 
+            Quaternion.identity);
+        droppedItem.rb2d.AddForce(spawnOffset * 1f, ForceMode2D.Impulse);
+        droppedGem = true;
+   }
 }
