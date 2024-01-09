2b6b189f10a85239159c909042f84eef09f22434
Kara Barrese <karabarrese@gmail.com>
2024-01-08 23:06:22 -0800

    Monkey & Gems
    monkey changes & drops axe... gems on floor end game! barney. christmas. america.

---
+++ EscapeTheCaveOfWonders/Assets/Scripts/Monkey.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Monkey : MonoBehaviour
+{
+    [SerializeField] public Sprite imgSprite;
+    public SpriteRenderer render;
+    private bool droppedAxe = false;
+
+    void Start()
+    {
+        Debug.Log("monkey business");
+        render = GetComponent<SpriteRenderer>();
+    }
+
+    void OnMouseDown()
+    {
+        if (!droppedAxe)
+        {
+            Debug.Log("clicked!");
+            render.sprite = imgSprite;
+
+            Item pickaxe = GameManager.instance.itemManager.GetItemByName("Pickaxe");
+            Vector2 spawnLocation = transform.position;
+            Vector2 spawnOffset = new Vector2(1f, Random.Range(-1f, 0f));
+
+            Item droppedItem = Instantiate(pickaxe, spawnLocation + spawnOffset, 
+                Quaternion.identity);
+            droppedItem.rb2d.AddForce(spawnOffset * 1f, ForceMode2D.Impulse);
+            droppedAxe = true;
+        
+        }
+
+    }
+}
