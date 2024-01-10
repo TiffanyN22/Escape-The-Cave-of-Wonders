86feea86c54a130b3702b126b3de2626385c52af
TiffanyN22 <tnguyen22@ndsj.org>
2024-01-10 09:25:20 -0800

    Click on treasure chest to get key

---
+++ EscapeTheCaveOfWonders/Assets/Scripts/TreasureChest.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureChest : MonoBehaviour
+{
+    [SerializeField] public Sprite imgSprite;
+    public SpriteRenderer render;
+    private bool droppedKey = false;
+
+    void Start()
+    {
+        render = GetComponent<SpriteRenderer>();
+    }
+
+    void OnMouseDown()
+    {
+        if (!droppedKey)
+        {
+            render.sprite = imgSprite;
+
+            Item key = GameManager.instance.itemManager.GetItemByName("Key");
+            Vector2 spawnLocation = transform.position;
+            Vector2 spawnOffset = new Vector2(1f, Random.Range(0f, 1f));
+
+            Item droppedItem = Instantiate(key, spawnLocation + spawnOffset, 
+                Quaternion.identity);
+            droppedItem.rb2d.AddForce(spawnOffset * 1f, ForceMode2D.Impulse);
+            droppedKey = true;
+        
+        }
+
+    }
+}
