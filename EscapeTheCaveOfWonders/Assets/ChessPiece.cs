a08b13072a50547b44c759409ae57ede98a4cf4e
Kara Barrese <karabarrese@gmail.com>
2024-01-15 23:18:48 -0800

    Chess Puzzle!

---
+++ EscapeTheCaveOfWonders/Assets/ChessPiece.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChessPiece : MonoBehaviour
+{
+    public Image image;
+    public Sprite imgSprite;
+    public int hi;
+    bool showImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // image = GetComponent<Image>();
+    }
+    public void updateImage(){
+     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
